77c08a9962c48782612e0e0b49015d7dcb6b6cd1
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-12-15 15:44:36 +0300

    add create plan route

---
+++ routes/planRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const { createPlan } = require('../controllers/planController');
+
+router.post('/create-plan', createPlan);
+
+module.exports = router;
