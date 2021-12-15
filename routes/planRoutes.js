b2b3999a46e5bbed61a9b8abbf1f19572d8d1b9a
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-12-15 15:50:40 +0300

    add get single plan route

--- routes/planRoutes.js
+++ routes/planRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const { createPlan } = require('../controllers/planController');
+const { createPlan, getPaymentPlan } = require('../controllers/planController');
 
 router.post('/create-plan', createPlan);
+router.get('/payment-plans/:id', getPaymentPlan);
 
 module.exports = router;
