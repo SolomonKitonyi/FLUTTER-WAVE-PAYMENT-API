176671ce951bac85fe4295d4e44affdc07c34181
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-12-15 15:53:29 +0300

    add get all plan route

--- routes/planRoutes.js
+++ routes/planRoutes.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const { createPlan, getPaymentPlan } = require('../controllers/planController');
+const {
+	createPlan,
+	getPaymentPlan,
+	getPaymentPlans,
+} = require('../controllers/planController');
 
 router.post('/create-plan', createPlan);
 router.get('/payment-plans/:id', getPaymentPlan);
+router.get('/payment-plans', getPaymentPlans);
 
 module.exports = router;
