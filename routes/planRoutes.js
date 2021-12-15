f33fa71c08238e77e69fc52f59b88fbf21147795
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-12-15 16:00:14 +0300

    add cancel plan route

--- routes/planRoutes.js
+++ routes/planRoutes.js
@@ -6,11 +6,13 @@ const {
 	getPaymentPlan,
 	getPaymentPlans,
 	updatePlan,
+	cancelPlan,
 } = require('../controllers/planController');
 
 router.post('/create-plan', createPlan);
 router.get('/payment-plans/:id', getPaymentPlan);
 router.get('/payment-plans', getPaymentPlans);
 router.put('/update-plan/:id', updatePlan);
+router.put('/cancel-plan/:id/cancel', cancelPlan);
 
 module.exports = router;
