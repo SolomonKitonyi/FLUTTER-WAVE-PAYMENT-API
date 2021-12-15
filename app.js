fcfce9e9fd94dbf46f476e1fe83fd000c458bd5d
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-12-15 15:43:23 +0300

    import plan routes

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ dotenv.config({ path: './config/config.env' });
 //middlewares
 app.use(express.json());
 
+//import routes
+const planRoutes = require('./routes/planRoutes');
+
+//routes middleware
+app.use('/api/v1', planRoutes);
+
 const port = process.env.PORT;
 app.listen(port, () => {
 	console.log(`Server running on Port ${port}`);
