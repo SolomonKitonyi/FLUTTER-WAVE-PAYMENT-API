3f117bbf307f45050e7dc542b76bd3740d8fd672
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-12-15 15:28:44 +0300

    set up app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const axios = require('axios');
+
+//setting up config file
+dotenv.config({ path: './config/config.env' });
+
+//middlewares
+app.use(express.json());
+
+const port = process.env.PORT;
+app.listen(port, () => {
+	console.log(`Server running on Port ${port}`);
+});
